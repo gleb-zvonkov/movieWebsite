8b96ff1e5d3d872598eb51af05428cb5883f9bd6
Gleb Zvonkov <glebzvonkov@Glebs-MacBook-Pro.local>
2025-06-03 17:55:18 -0600

    cors policy

--- backend/index.js
+++ backend/index.js
@@ -35,8 +35,19 @@ const PORT = 5050;
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite frontend URL
-    credentials: true, // allow credentials like cookies or auth headers
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:5173",
+        "https://coralwindow.ca",
+        "https://gleb-zvonkov.github.io",
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
   })
 );
 
